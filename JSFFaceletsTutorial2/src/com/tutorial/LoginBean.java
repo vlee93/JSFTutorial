c7704a507389b630442055a3aa978bfd97055553
Victoria Lee <vlee93@gmail.com>
2015-09-09 11:53:16 -0400

    First Commit

---
+++ JSFFaceletsTutorial2/src/com/tutorial/LoginBean.java
@@ -0,0 +1,34 @@
+package com.tutorial;
+
+public class LoginBean {
+	private String name;
+
+	private String password;
+	private String favcolor;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+	
+	public String getFavcolor() {
+		return favcolor;
+	}
+
+	public void setFavcolor(String favcolor) {
+		this.favcolor = favcolor;
+	}
+
+
+}
